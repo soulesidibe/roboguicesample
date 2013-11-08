e2376dc860f6178178adefb43865bf7d9749e913
Souleymane Sidibe <souleymane.sidibe01@gmail.com>
2013-11-08 17:13:06 +0000

    done

--- Roboguicesample/src/main/java/sn/soulesidibe/sample/roboguice/activity/MainActivity.java
+++ Roboguicesample/src/main/java/sn/soulesidibe/sample/roboguice/activity/MainActivity.java
@@ -1,34 +1,49 @@
-package sn.soulesidibe.sample.roboguice;
+package sn.soulesidibe.sample.roboguice.activity;
 
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectResource;
+import roboguice.inject.InjectView;
+import sn.soulesidibe.sample.roboguice.R;
+
+public class MainActivity extends RoboActivity {
+
+    @InjectView(R.id.edt_name)
+    EditText name;
+    @InjectView(R.id.edt_passwd)
+    EditText passwd;
+    @InjectView(R.id.btn_connect)
+    Button signin;
+
+    @InjectResource(R.string.str_welcome)
+    String welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String strName = " " + name.getText().toString();
+                Toast.makeText(MainActivity.this, welcome + strName, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -44,22 +59,6 @@ public class MainActivity extends Activity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 
 }
